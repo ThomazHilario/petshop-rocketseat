aaa7ddd3d5beb683dc1992daafa7f1c87beb21ee
Thomaz Alves <thomazhilario5@gmail.com>
2026-08-25 15:50:57 +0000

    fix: types localstorage.

--- src/utils/useLocalStorage.tsx
+++ src/utils/useLocalStorage.tsx
@@ -4,7 +4,8 @@ export const setLocalStorage = (key: string, value: unknown) =>
 export const getLocalStorage = (key: string) => {
   const appointments = localStorage.getItem('appointments');
 
-  const parsed = JSON.parse(appointments!);
-
-  return parsed;
+  if (appointments) {
+    const parsed = JSON.parse(appointments);
+    return parsed;
+  }
 };
