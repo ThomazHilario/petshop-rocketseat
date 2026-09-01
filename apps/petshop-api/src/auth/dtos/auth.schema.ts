d6dfc9f06917258997df95ae72933ff950eb0e81
Thomaz Alves <thomazhilario5@gmail.com>
2026-09-01 14:24:24 +0000

    feat: create regex for password.

--- apps/petshop-api/src/auth/dtos/auth.schema.ts
+++ apps/petshop-api/src/auth/dtos/auth.schema.ts
@@ -8,6 +8,8 @@ export class AuthLoginDTO {
 
     @IsString()
     @IsNotEmpty()
-    @Matches()
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
+        message: "Password must contain at least 8 characters, including one uppercase letter, one lowercase letter, one number and one special character"
+    })
     password: string;
 };
